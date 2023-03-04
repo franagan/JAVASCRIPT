160593c2c12dc86e1ea18e4db81277c1d54223cf
franagan <118491675+franagan@users.noreply.github.com>
2023-03-04 20:16:13 +0100

    jss

---
+++ CLASE 1/CLASE1/main.js
@@ -0,0 +1,25 @@
+// console.log('Hello world')
+// alert('hola mundo')
+//comentario
+/*comentarios
+*/
+//null
+
+//Undefined
+// let noAsignado;
+// console.log(noAsignado);
+
+//interacciones
+// let tienesMascota; 
+// tienesMascota= confirm('¿Tienes mascota?');
+// console.log('Has contestado' + tienes mascota);
+
+// let nombreMascota = prompt ('como se llama tu mascota?', '');
+
+// let edadPerro = prompt ('Cuantos años tiene tu perro')
+
+// edadHombre = edadPerro*7;
+
+// let edadPerroHumana = edadHombre;
+
+// console.log ('Tu perro' + nombreMascota + ' tiene ' + edadPerroHumana + 'años humanos');
